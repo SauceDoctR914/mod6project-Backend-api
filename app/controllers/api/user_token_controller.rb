cdabedf67bfd1a41de9d4ee6feae5d0840375e8c
GavIn Sokolof <sokolofg@gmail.com>
2019-04-17 15:45:31 -0400

    changed typo in notebooks controller create

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,3 +1,13 @@
 class Api::UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
+
+
+  def create
+    auth = auth_token
+    if auth
+      user_obj = User.find_by(email: params["auth"]["email"])
+    render json: {jwt: auth.token, email: user_obj.email}, status: :created
+  end
+  end
+
 end
